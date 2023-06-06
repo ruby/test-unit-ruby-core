ba138ce47de49da9073af2844818ef4b14efd033
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-06-06 16:12:29 +0900

    bundle gem test-unit-ruby-core

---
+++ lib/test/unit/ruby/core.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "core/version"
+
+module Test
+  module Unit
+    module Ruby
+      module Core
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/test/unit/ruby/core/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Test
+  module Unit
+    module Ruby
+      module Core
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
